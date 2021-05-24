b7807def6fa4f7c5c7e3806d3ee5a43558ab23ee
HuangPingyang1 <1797417740@qq.com>
2021-05-24 16:25:40 +0800

    build Mon May 24 16:25:40 CST 2021

--- push_git.sh
+++ push_git.sh
@@ -2,6 +2,9 @@
 
 cd /srv/HuangPingyang1.github.io/
 git pull origin main
-git add .
-git commit -m "build `date`"
-git push origin main
+if [ $? -eq 0 ]
+then
+	git add .
+	git commit -m "build `date`"
+	git push origin main
+fi
